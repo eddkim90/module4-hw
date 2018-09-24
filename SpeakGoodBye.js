ce46e7c0e06d5b11a0cd257eb40fe7e9893615bb
eddkim90 <edprodigy07@gmail.com>
2018-09-24 08:13:01 -0400

    Removed SpeakSimple object

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -10,7 +10,6 @@
     // the "speak" method and which you will expose to the global context
     // See Lecture 52, part 1
     var byeSpeaker = {};
-    var speakSimple = {};
 
     // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
     var speakWord = "Good Bye";
